1d71181d1bb4c595bdf83bdfba2e09479e4886fa
Nerbyk <danil.maximov@flatstack.com>
2022-05-11 03:31:19 +0200

    handle multiple connections

--- app/server.rb
+++ app/server.rb
@@ -3,26 +3,37 @@ require 'socket'
 class YourRedisServer
   def initialize(port)
     @server = TCPServer.new(port)
+    @clients = []
     puts "Listening on port #{port}"
   end
 
   def start
+    Thread.new do
+      loop do
+        new_client = @server.accept
+        @clients << new_client
+      end
+    end
+
     loop do
-      connection = @server.accept 
-      handle(connection)
-      puts("CLOSED")
+      sleep(0.1) if @clients.empty?
+
+      @clients.each_with_index do |client|
+        client_command = client.read_nonblock(256, exception: false)
+
+        @clients.delete(client) if client.closed?
+        next if client_command == :wait_readable || client_command.nil?
+
+        response = handle_client_command(client_command)
+        client.puts response
+      end
     end
   end
 
   private
 
-  def handle(connection)
-    until connection.eof?
-      request = connection.gets
-
-      connection.close if request.nil?
-      connection.puts('+PONG') unless connection.closed?
-    end
+  def handle_client_command(_command)
+    "+PONG"
   end
 end
 
